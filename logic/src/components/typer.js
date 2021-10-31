c5982fb693987afd7b3a2d8e09b09ae6da42a0cf
JasonHangZhou <jhfnetboy@gmail.com>
2021-10-31 17:43:47 +0800

    选动态响应

--- logic/src/components/typer.js
+++ logic/src/components/typer.js
@@ -7,7 +7,7 @@ import craft from '../game-data/craft.json';
 // interface property {str: string[]}
 // const props
 const str = [
-    '`当你结束了一天疲惫的工作回到独居的家中，`<br/>`打开家门却意外地发现自己的爱犬 *DogName `<br/>`没有像往常一样晃着尾巴出现在门口迎接你。`<br/>`只见地上留了一张质地很古怪的便签：`<br/>很抱歉没办法提前告知，<br/>但哈迪斯大人要求我尽快将*DogName带回冥界。<br/>如您对此有任何意见，欢迎您拜访以下地址与哈迪斯大人当面沟通…… <br/>——塔纳托斯',
+    '`当你结束了一天疲惫的工作回到独居的家中，`<br/>`打开家门却意外地发现自己的爱犬 *DogName `<br/>`没有像往常一样晃着尾巴出现在门口迎接你。`<br/>`只见地上留了一张质地很古怪的便签：`<br/>很抱歉没办法提前告知，<br/>但哈迪斯大人要求我尽快将*DogName带回冥界。<br/>如您对此有任何意见，欢迎您拜访以下地址与哈迪斯大人当面沟通…… <br/>——塔纳托斯^2000',
     'a. 我有一种不祥的预感，不然就算了？<br/>b. 我有计划了。赶紧准备一下就前往冥界。<br/>c. 什么鬼？立刻出发！'
   ]
 
@@ -18,11 +18,14 @@ const TypedReactHooksDemo = () => {
 	const typed = React.useRef(null);
     // const [fetchWalkerName] = useWalker({ web3 })
     const [lastPressedKey, setLastPressedKey] = useState()
+    const [currentStr, setCurrentStr] = useState(str)
+
     const handleFetch = async () => {
         // fetchWalkerName(walkerID).then(setWalker).catch(showAppMsg)
       }
   React.useEffect(() => {
     const options = {
+    // strings : getCurrentStr(),
     strings : str,
     typeSpeed: 30,
     backSpeed: 0,
@@ -33,31 +36,41 @@ const TypedReactHooksDemo = () => {
     
     // elRef refers to the <span> rendered below
     typed.current = new Typed(el.current, options);
-    
     window.addEventListener("keydown", handleKeyPress);
 
     return () => {
-      // Make sure to destroy Typed instance during cleanup
-      // to prevent memory leaks
       typed.current.destroy();
     }
   }, [])
-  
-//   const componentDidMount= ()=> {
-//     window.addEventListener("keydown", handleKeyPress);
-//   }
-//   const componentWillUnmount=()=> {
-//     window.removeEventListener("keydown", handleKeyPress);
-//   }
+
+  const HandleCurrentStr = ()=>{
+    setCurrentStr()
+  }
+
+  function freshStr(newStr){
+    const options = {
+        // strings : getCurrentStr(),
+        strings : newStr,
+        typeSpeed: 30,
+        backSpeed: 0,
+        cursorChar: '_',
+        fadeOut: true,
+        shuffle: true,
+        };
+        
+        // elRef refers to the <span> rendered below
+        typed.current = new Typed(el.current, options);
+        return typed.current
+  }
+
   const handleKeyPress = (event) => {
     setLastPressedKey(event.key);
-    // console.log("Press key",event.key)
-    // console.log("Press keyTrue",event.code)
-    // console.log("Press keyCode",event.keyCode)
-    // console.log((event.key).toString())
     switch ((event.key).toString()) {
         case "a":
             console.log("i am a touched")
+            typed.current.destroy()
+            typed.current = freshStr(["你选择了a","狗狗来了"])
+            typed.current.reset()
             break;
         case "b":
             console.log("i am b touched")
