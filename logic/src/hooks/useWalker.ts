9a749b2529cc6ce3ab083d0befe45cd135b5a8af
JasonHangZhou <jhfnetboy@gmail.com>
2021-11-01 09:57:29 +0800

    page config

--- logic/src/hooks/useWalker.ts
+++ logic/src/hooks/useWalker.ts
@@ -1,4 +1,3 @@
-import { ethers, providers } from "ethers";
 import { useEffect, useState } from "react";
 require('dotenv').config();
 // const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -15,76 +14,39 @@ const contractAddress = (networkName: string) => {
     return networkName === "maticmum" ? CONTRACT_ADDRESS : localAddress
 }
 
-type Props = {
-  web3: providers.Web3Provider | undefined
-}
+// type Props = {
+//   web3: providers.Web3Provider | undefined
+// }
 
-function useWalker({ web3 }: Props) {
+// function useWalker({ web3 }: Props) {
+  function useWalker() {
   const [address, setAddress] = useState<string>(localAddress)
 
-  useEffect(() => {
-    web3?.detectNetwork().then((network) => {
-      const address_ = contractAddress(network.name)
-      setAddress(address_)
-    })
-  }, [web3])
+  // useEffect(() => {
+  //   web3?.detectNetwork().then((network) => {
+  //     const address_ = contractAddress(network.name)
+  //     setAddress(address_)
+  //   })
+  // }, [web3])
 
   const fetchWalkerName = async (walkerID: number) => {
-    if (typeof web3 === "undefined") return
+    // if (typeof web3 === "undefined") return
 
-    const contract = new ethers.Contract(address, Walker.abi, web3)
+    // const contract = new ethers.Contract(address, Walker.abi, web3)
 
-    try {
-      const data = await contract.tokenWalkerName(walkerID)
-      console.log("data: ", data)
-      return data
-    } catch (err) {
-      console.log("Error: ", err)
-      throw err
-    }
+    // try {
+    //   const data = await contract.tokenWalkerName(walkerID)
+    //   console.log("data: ", data)
+    //   return data
+    // } catch (err) {
+    //   console.log("Error: ", err)
+    //   throw err
+    // }
   }
 
-  const setWalkerName = async (walkerID: number, walkerName: string) => {
-    if (typeof web3 === "undefined") return
-    if (!setWalkerName) throw new Error("Record Empty")
+  
 
-    const signer = web3.getSigner()
-    const contract = new ethers.Contract(address, Walker.abi, signer)
-    const transaction = await contract._setTokenName(walkerID, walkerName)
-    await transaction.wait()
-  }
-
-  const mintWalker = async (mint_address: string, walkerName: string) => {
-    if (typeof web3 === "undefined") return
-    if (!setWalkerName) throw new Error("Record Empty")
-
-    const signer = web3.getSigner()
-    const contract = new ethers.Contract(address, Walker.abi, signer)
-    try{
-      const transaction = await contract.mint(mint_address, walkerName)
-      await transaction.wait()
-      console.log("Tx: ", transaction)
-      return 
-    }catch (err) {
-      console.log("Error: ", err)
-      throw err
-    }
-  }
-
-  const getNftCount = async (wallet_address: string) => {
-    if (typeof web3 === "undefined") return
-    if (!setWalkerName) throw new Error("Record Empty")
-
-    const signer = web3.getSigner()
-    const contract = new ethers.Contract(address, Walker.abi, signer)
-    console.log("userwork invoke: "+wallet_address)
-    const count = await contract.balanceOf(wallet_address)
-    console.log(count)
-    // await count.wait()
-    return parseInt(count,16)
-  }  
-
-  return [fetchWalkerName, setWalkerName, mintWalker, getNftCount] as const
+  return [fetchWalkerName] as const
 }
 
 export default useWalker
